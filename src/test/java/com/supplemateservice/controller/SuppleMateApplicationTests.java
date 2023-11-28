618c7519a3a6a1e709ea342b23e8ad57b345c1aa
sidkrish03 <ss3875@njit.edu>
2023-11-27 21:15:30 -0600

    Test - Added test cases for supplement entry data layer functionalities

--- src/test/java/com/supplemateservice/controller/SuppleMateApplicationTests.java
+++ src/test/java/com/supplemateservice/controller/SuppleMateApplicationTests.java
@@ -1,4 +1,4 @@
-package com.supplemateservice;
+package com.supplemateservice.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ src/test/java/data/SupplementEntryDaoTest.java
@@ -0,0 +1,85 @@
+package data;
+
+import com.supplemateservice.data.SupplementEntryDao;
+import com.supplemateservice.model.SupplementEntry;
+import org.junit.jupiter.api.*;
+
+import java.util.List;
+
+public class SupplementEntryDaoTest {
+    public SupplementEntryDaoTest() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addSupplementEntry method, of class SupplementEntryDao.
+     */
+    @Test
+    public void testAddSupplementEntry() {
+    }
+
+    /**
+     * Test of getSupplementEntryById method, of class SupplementEntryDao.
+     */
+    @Test
+    public void testGetSupplementEntryById() {
+    }
+
+    /**
+     * Test of getAllSupplementEntriesSorted method, of class SupplementEntryDao.
+     */
+    @Test
+    public void testGetAllSupplementEntriesSorted() {
+    }
+
+    /**
+     * Test of editSupplementEntry method, of class SupplementEntryDao.
+     */
+    @Test
+    public void testEditSupplementEntry() {
+    }
+
+    /**
+     * Test of deleteSupplementEntry method, of class SupplementEntryDao.
+     */
+    @Test
+    public void testDeleteSupplementEntry() {
+    }
+
+    public class SupplementEntryDaoImpl implements SupplementEntryDao {
+
+        public SupplementEntry addSupplementEntry(SupplementEntry supplementEntry) {
+            return null;
+        }
+
+        public SupplementEntry getSupplementEntryById(int supplementEntryId) {
+            return null;
+        }
+
+        public List<SupplementEntry> getAllSupplementEntriesSorted() {
+            return null;
+        }
+
+        public SupplementEntry editSupplementEntry(SupplementEntry updatedSupplementEntry) {
+            return null;
+        }
+
+        public void deleteSupplementEntry(int supplementEntryId) {
+        }
+    }
+}
